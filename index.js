c223d23a091be151a7dd3a3801de15bf4d365ce5
sibesh1 <sibesh1998@gmail.com>
2020-12-15 21:45:51 +0530

    added current command

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
 
+const API_KEY = process.env.API_KEY;
 const token = process.env.TOKEN;
 
 // Created instance of TelegramBot
@@ -11,15 +13,25 @@ const bot = new TelegramBot(token, {
 // Listener (handler) for telegram's /info event
 bot.onText(/\/info/, (msg, match) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Bot By Sibesh Behera");
+  bot.sendMessage(chatId, "Bot By Sibesh Behera\nhttps://github.com/sibesh1");
+});
+
+// Listener (handler) for telegram's /start event
+bot.onText(/\/start/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(
+    chatId,
+    "You can use this bot by typing in the below specified format:\n1)/info-Gives info of owner of this bot.\n2)/current <Enter the name of location> - Gives current weather details of the location.\n3)/forecast <Enter the name of location> - Gives weather forecast of the location."
+  );
 });
 
 // Listener (handler) for telegram's /location event
-bot.onText(/\/location/, (msg, match) => {
+bot.onText(/\/current/, (msg, match) => {
   const chatId = msg.chat.id;
-  console.log(match);
+  //console.log(match);
   const location = match.input.split(" ")[1];
-  console.log(location);
+  const weatherurl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
+  //console.log(location);
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -29,5 +41,24 @@ bot.onText(/\/location/, (msg, match) => {
     return;
   }
 
-  bot.sendMessage(chatId, `You enetred ${location}`);
+  let data = [];
+
+  axios
+    .get(weatherurl)
+    .then(function (response) {
+      // handle success
+      data = response.data;
+      //   console.log(
+      //     `Your location is ${data.location.name},${data.location.country}.\nThe current temperature is ${data.current.temp_c}C and feelslike ${data.current.feelslike_c}C.\nThe windspeed is ${data.current.wind_kph}kph.`
+      //   );
+      bot.sendMessage(
+        chatId,
+        `Your location is ${data.location.name},${data.location.country}.\nThe current temperature is ${data.current.temp_c}\u00B0C and feelslike ${data.current.feelslike_c}\u00B0C.\nThe windspeed is ${data.current.wind_kph}kph.`
+      );
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      bot.sendMessage("Some error occurred.I will rectify it soon.");
+    });
 });
